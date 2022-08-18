2cc38a20e980b62befae339dfed728f45f96a482
Wayne <52121443+kddco@users.noreply.github.com>
2022-08-18 19:16:06 +0800

    reload tor

--- torLoop.py
+++ torLoop.py
@@ -1,3 +1,10 @@
-# while(True):
-import subprocess
-print(subprocess.call(["ls", "-l"]))
+
+# import subprocess
+# print(subprocess.call(["ls", "-l"]))
+
+import os
+while(True):
+    print(os.system("service tor reload"))
+
+    print(os.system("curl https://challenge.uccuhacker.tw/nop.php?token=MzZ8a2RkY28=_f7d663f38bc772b661937cbdd3c4a7656393952c98745bfd5455eb6bc0be09d7"))
+    
